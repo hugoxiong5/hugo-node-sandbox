7d2313eaed568fdb5f0b0842a17b8c250ef0a575
Hugo Xiong <hugoxiong@gmail.com>
2020-03-14 23:55:07 -0700

    added Peaksay form get

--- routes/peaksay-routes.js
+++ routes/peaksay-routes.js
@@ -1,3 +1,5 @@
+var conversation_controller = require('../controllers/conversationController');
+
 var express = require('express');
 
 var router = express.Router();
@@ -13,6 +15,15 @@ router.get('/', function(req, res) {
 router.get('/learn', function(req, res) {
     res.sendFile( __rootdir + "/public/" + "peaksay-learn.html");
   });
+
+// Peaksay: create conversation form
+
+// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
+router.get('/create', conversation_controller.convo_create_get);
+
+// // POST request for creating Book.
+router.post('/create', conversation_controller.convo_create_post);
+
 
 module.exports = router;
 
